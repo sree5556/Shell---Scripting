9c091bfc750907cc6bf5470fd2f59e40197c6c37
SriNivas <sreenivasyandamuri44@gmail.com>
2020-09-12 18:58:15 +0530

    mongod debugging start

--- catalogue.sh
+++ catalogue.sh
@@ -66,7 +66,7 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-         
+         status_check
          ;;
        *)
          echo "program invalid"

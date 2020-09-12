1029f2ae46c7780e2b72351f2d9801fc3f9b30b1
SriNivas <sreenivasyandamuri44@gmail.com>
2020-09-12 18:55:43 +0530

    mongod start

--- catalogue.sh
+++ catalogue.sh
@@ -56,8 +56,22 @@ case $1 in
          systemctl enable nginx
          systemctl start nginx
          ;;
+       mongod)
+         status_check
+         Print "  Starting of Mongod "
+         Print "settinng of repos"
+         echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+         
+         ;;
        *)
          echo "program invalid"
+
+         ;;
 
 
 

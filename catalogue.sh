6ee81ec947eb5187df672d02795c98345c780555
SriNivas <sreenivasyandamuri44@gmail.com>
2020-09-12 19:22:30 +0530

    mongod  unzip

--- catalogue.sh
+++ catalogue.sh
@@ -82,7 +82,13 @@ gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mong
          Print "Bind ip- changed"
          status_check
          systemctl restart mongod
-
+         Print"Downloading the schema"
+         curl -s -L -o /tmp/mongodb.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/e9218aed-a297-4945-9ddc-94156bd81427/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+         status_check
+         cd /tmp
+         Print "Extracting the unzip file"
+         unzip -o mongodb.zip
+         status_check
          ;;
        *)
          echo "program invalid"

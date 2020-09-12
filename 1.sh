95e8e1b38368e99c6ebfb662394be3c1daea7761
SriNivas <sreenivasyandamuri44@gmail.com>
2020-09-12 16:40:33 +0530

    not updated

--- 1.sh
+++ 1.sh
@@ -81,6 +81,10 @@ satus_check(){
               Print "Starting MongoDB Service"
               systemctl enable mongod
               systemctl start mongod
+              Pint"changing the configuration file with 0.0.0.0 /etc/mongod.conf"
+              sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+              satus_check
+              systemctl restart mongod
 
               Print"Downloading the Schema"
               curl -s -L -o /tmp/mongodb.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/e9218aed-a297-4945-9ddc-94156bd81427/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
@@ -91,6 +95,7 @@ satus_check(){
               mongod < catalogue.js
               Print"load users schema"
               mongod < users.js
+              Print"installation completed"
 
             ;;
     catalogue)

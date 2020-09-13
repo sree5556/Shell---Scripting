08b09575aa60794c27e8c1e4b45d629e5a38a248
SriNivas <sreenivasyandamuri44@gmail.com>
2020-09-13 20:13:40 +0530

    cata

--- cata.sh
+++ cata.sh
@@ -49,10 +49,10 @@ cd /home/roboshop
 status_check
 
 Print "Downloading the schema"
-curl -s -L -o /tmp/user.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/558568c8-174a-4076-af6c-51bf129e93bb/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
-mkdir user
-cd user
-unzip -o /tmp/user.zip
+curl -s -L -o /tmp/catalogue.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/558568c8-174a-4076-af6c-51bf129e93bb/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+mkdir catalogue
+cd catalogue
+unzip -o /tmp/catalogue.zip
 status_check
 Print "Downloading the npm"
 npm install
@@ -61,6 +61,6 @@ status_check
 cd
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
-systemctl start user
-systemctl enable user
+systemctl start catalogue
+systemctl enable catalogue
 status_check
